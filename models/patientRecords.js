cb0ce4499897a860f36bcd6d4e8acc7fd7d16142
aszhangUOM <aszhang@student.unimelb.edu.au>
2022-04-27 21:36:11 +1000

    changed tables in clinicianDashboard hbs

--- models/patientRecords.js
+++ models/patientRecords.js
@@ -10,43 +10,32 @@ const PatientRecord = mongoose.model('patientRecord', schema)
 module.exports = PatientRecord
 
 
-//
-//
-//const mongoose = require('mongoose')
-//
-//const schema = new mongoose.Schema({
-//  record_type: String,
-//  value: String,
-//  username: String,
-//})
-//
-//const PatientRecord = mongoose.model('patientRecord', schema)
-//module.exports = PatientRecord
-//
-//
-//
-//
-//var patientCollection = require('./patients');//import user model file
-//var resources = {
-//  username: "$nick_name"};
-//
-//userCollection.aggregate([{
+
+
+
+// var patientCollection = require('./patients');//import user model file
+// var resources = {
+//  username: "$username"};
+
+// userCollection.aggregate([{
 //  $group: resources
-//}, {
+// }, {
 //  $lookup: {
-//      from: "Comments", // collection to join
+//      from: "patientRecords", // collection to join
 //      localField: "_id",//field from the input documents
-//      foreignField: "user_id",//field from the documents of the "from" collection
+//      foreignField: "username",//field from the documents of the "from" collection
 //      as: "comments"// output array field
 //  }
-//}, {
+// }, {
 //  $lookup: {
 //      from: "Post", // from collection name
 //      localField: "_id",
 //      foreignField: "user_id",
 //      as: "posts"
 //  }
-//}],function (error, data) {
+// }],function (error, data) {
 //  return res.json(data);
 //  //handle error case also
-//});
+// });
+
+
