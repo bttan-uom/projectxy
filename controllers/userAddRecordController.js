884aea204edb8c5c46fa21f6d61787eb15be0790
Brad Tan <bradtan@Brads-MacBook-Pro.local>
2022-04-27 12:43:11 +1000

    make dashboard easier to read on mobile by reducing information box size

--- controllers/userAddRecordController.js
+++ controllers/userAddRecordController.js
@@ -3,27 +3,37 @@
 const Author = require('../models/patientRecords')
 //const peopleData = require('../models/peopleModel')
 
-// CHALLENGE EXERCISE: change this to use MongoDB!
-// const insertData = (req, res) => {
-//     const { id, first_name, last_name } = req.body
-//     peopleData.push({ id, first_name, last_name })
-//     return res.redirect('back')
-// }
+
 const getAddUserRecordsPage = (req, res) => {
     res.render('userAddRecord')
 }
 
-// CHALLENGE solution
-// note: This function is omitted from my solution on Heroku
+
 const addNewUserRecord = async (req, res, next) => {
     try {
         newAuthor = new Author( req.body )
         await newAuthor.save()
-        res.render('userAddRecordSuccess')
+        res.render('userAddRecordSuccess', {oneItem: author})
     } catch (err) {
         return next(err)
     }
 }
+
+//const getDataById = async (req, res, next) => {
+//  // search the database by ID
+//  try {
+//      const author = await Author.findById(req.params.patientRecord_id).lean()
+//      if (!author) {
+//          // no author found in database
+//          return res.sendStatus(404)
+//      }
+//      // found the author
+//      return res.render('oneData', {oneItem: author})
+//  } catch (err) {
+//      return next(err)
+//  }
+//  
+//}
 
 // exports an object, which contain functions imported by router
 module.exports = {
