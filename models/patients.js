576281eba3cba3a07a5020b2698e0cac22ee9220
aszhangUOM <aszhang@student.unimelb.edu.au>
2022-04-27 15:08:26 +1000

    made changes to controllers and patient schema

--- models/patients.js
+++ models/patients.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    
+    username: String,
     first_name: String,
-    last_name: String
+    last_name: String,
+    email: String,
+    phone: String,
+    height: Decimal128,
+    address: String,
+    gender: String,
+    dob: Date,
+    signupdate: Date,
+    clinician_username: String
 })
 
 const Author = mongoose.model('patients', schema)
