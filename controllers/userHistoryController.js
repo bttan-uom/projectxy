a0ccfa7a43876f5466d4fe2171eda7664316dd26
Rory Healy <roryihealy@outlook.com>
2022-04-28 21:17:11 +1000

    added clinician name to individual patient records

--- controllers/userHistoryController.js
+++ controllers/userHistoryController.js
@@ -23,13 +23,21 @@ const getAllHistory = async (req, res, next) => {
 const getDataById = async (req, res, next) => {
     // search the database by ID
     try {
-        const author = await Records.findById(req.params.patientRecord_id).lean()
-        if (!author) {
+        const record = await Records.findById(req.params.patientRecord_id).lean()
+        if (!record) {
             // no author found in database
             return res.sendStatus(404)
         }
         // found the author
-        return res.render('oneData', {oneItem: author})
+
+        // Hard-coded email for example in deliverable 2.
+        // Not to be used in deliverable 3.
+        const clinician = await joins.getClinician('pat.fakename@example.com')
+        if (!clinician) {
+            // Patient does not have a clinician
+            return res.sendStatus(404)
+        }
+        return res.render('oneData', {oneItem: record, clinician: clinician})
     } catch (err) {
         return next(err)
     }
@@ -39,5 +47,5 @@ const getDataById = async (req, res, next) => {
 // exports an object, which contain functions imported by router
 module.exports = {
     getAllHistory,
-    getDataById,
+    getDataById
 }
