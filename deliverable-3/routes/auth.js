242a5a302e0bcd1f7b39f2909eefff6c94b8fa9c
aszhangUOM <aszhang@student.unimelb.edu.au>
2022-05-11 00:02:18 +1000

    completed authentication of user dashboard with db data loading

--- deliverable-3/routes/auth.js
+++ deliverable-3/routes/auth.js
@@ -1,36 +1,45 @@
 const passport = require('passport')
 const express = require('express')
 const router = express.Router()
+const userDashboardRouter = express.Router()
+const userDashboardController = require('../controllers/userDashboardController')
+
 // Authentication middleware
 const isAuthenticated = (req, res, next) => {
-// If user is not authenticated via passport, redirect to login page
-if (!req.isAuthenticated()) {
-2
-return res.redirect('/login')
+    // If user is not authenticated via passport, redirect to login page
+    if (!req.isAuthenticated()) {
+        return res.redirect('/login')
+    }
+    // Otherwise, proceed to next middleware function
+    return next()
 }
-// Otherwise, proceed to next middleware function
-return next()
-}
+
 // Main page which requires login to access
 // Note use of authentication middleware here
-router.get('/', isAuthenticated, (req, res) => {
-res.render('index', { title: 'Express', user: req.user })
-})
+// router.get('/', isAuthenticated, (req, res) => {
+//     res.render('index', { title: 'Express', user: req.user })
+    
+// })
+
+
 // Login page (with failure message displayed upon login failure)
 router.get('/login', (req, res) => {
-res.render('login', { flash: req.flash('error'), title: 'Login' })
+    res.render('login', { flash: req.flash('error'), title: 'Login' })
 })
+
 // Handle login
 router.post('/login',
-passport.authenticate('local', {
-successRedirect: '/', failureRedirect: '/login', failureFlash: true
-})
+    passport.authenticate('local', {
+        successRedirect: '/', failureRedirect: '/login', failureFlash: true
+    })
 )
+
 // Handle logout
 router.post('/logout', (req, res) => {
-req.logout()
-res.redirect('/')
+    req.logout()
+    res.redirect('/')
 })
 
 module.exports = router
+
 
--- deliverable-3/routes/userDashboardRouter.js
+++ deliverable-3/routes/userDashboardRouter.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('passport')
 
 // create our Router object
 const userDashboardRouter = express.Router()
@@ -6,11 +7,22 @@ const userDashboardRouter = express.Router()
 // import people controller functions
 const userDashboardController = require('../controllers/userDashboardController')
 
+
+// Authentication middleware
+const isAuthenticated = (req, res, next) => {
+    // If user is not authenticated via passport, redirect to login page
+    if (!req.isAuthenticated()) {
+        return res.redirect('/login')
+    }
+    // Otherwise, proceed to next middleware function
+    return next()
+}
+
 // add a route to handle the GET request for all people data
-userDashboardRouter.get('/', userDashboardController.getAllRecords)
+userDashboardRouter.get('/', isAuthenticated, userDashboardController.getAllRecords)
 
 // add a route to handle the GET request for one data instance
-userDashboardRouter.get('/:patientRecord_id', userDashboardController.getDataById)
+userDashboardRouter.get('/:patientRecord_id', isAuthenticated, userDashboardController.getDataById)
 
 // export the router
 module.exports = userDashboardRouter
