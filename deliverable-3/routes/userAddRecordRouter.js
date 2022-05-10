149c2de118c95fd825dec6adee0c012ab05157e9
aszhangUOM <aszhang@student.unimelb.edu.au>
2022-05-11 00:16:29 +1000

    added authentication to user add record page

--- deliverable-3/routes/userAddRecordRouter.js
+++ deliverable-3/routes/userAddRecordRouter.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('passport')
 
 // create our Router object
 const userAddRecordRouter = express.Router()
@@ -6,11 +7,21 @@ const userAddRecordRouter = express.Router()
 // import controller functions
 const userAddRecordController = require('../controllers/userAddRecordController')
 
+// Authentication middleware
+const isAuthenticated = (req, res, next) => {
+    // If user is not authenticated via passport, redirect to login page
+    if (!req.isAuthenticated()) {
+        return res.redirect('/login')
+    }
+    // Otherwise, proceed to next middleware function
+    return next()
+}
+
 // add a route to handle the GET request for add records page
-userAddRecordRouter.get('/', userAddRecordController.getAddUserRecordsPage)
+userAddRecordRouter.get('/', isAuthenticated, userAddRecordController.getAddUserRecordsPage)
 
 // add a new JSON object to the database
-userAddRecordRouter.post('/', userAddRecordController.addNewUserRecord)
+userAddRecordRouter.post('/', isAuthenticated, userAddRecordController.addNewUserRecord)
 
 // export the router
 module.exports = userAddRecordRouter
--- deliverable-3/routes/userHistoryRouter.js
+++ deliverable-3/routes/userHistoryRouter.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('passport')
 
 // create our Router object
 const userHistoryRouter = express.Router()
@@ -6,11 +7,20 @@ const userHistoryRouter = express.Router()
 // import people controller functions
 const userHistoryController = require('../controllers/userHistoryController')
 
+const isAuthenticated = (req, res, next) => {
+    // If user is not authenticated via passport, redirect to login page
+    if (!req.isAuthenticated()) {
+        return res.redirect('/login')
+    }
+    // Otherwise, proceed to next middleware function
+    return next()
+}
+
 // add a route to handle the GET request for all people data
-userHistoryRouter.get('/', userHistoryController.getAllHistory)
+userHistoryRouter.get('/', isAuthenticated, userHistoryController.getAllHistory)
 
 // add a route to handle the GET request for one data instance
-userHistoryRouter.get('/:patientRecord_id', userHistoryController.getDataById)
+userHistoryRouter.get('/:patientRecord_id', isAuthenticated,  userHistoryController.getDataById)
 
 // export the router
 module.exports = userHistoryRouter
